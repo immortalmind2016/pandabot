c1641fb428d0a9dcb06258fad78d588076ae8283
immortalmind2016 <immortal.mind2016@gmail.com>
2019-08-20 21:02:29 +0200

    init

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ if(cluster.isMaster){
          app.use(function(req, res, next) {
     
                 res.setHeader("Access-Control-Allow-Origin", "*");
+                res.setHeader("Access-Control-Allow-Origin", "https://d2a2bac9.ngrok.io");
 
                res.setHeader("Access-Control-Allow-Methods", "POST, GET,DELETE,OPTIONS");
                res.setHeader("Access-Control-Max-Age", "3600");

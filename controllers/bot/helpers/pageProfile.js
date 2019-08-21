5cd630e025315ae91d8fcfa6c8f3d7e74a7b1faa
immortalmind2016 <immortal.mind2016@gmail.com>
2019-08-21 10:22:29 +0200

    init

--- controllers/bot/helpers/pageProfile.js
+++ controllers/bot/helpers/pageProfile.js
@@ -87,9 +87,8 @@ const sendMessage=(senderId,accessToken,templates,i=0)=>{
                 let message=templates[i].message
                 if(!!templates[i].message){
                     for(var v=0;v<vars.length;v++){
-                        console.log("V ",user[vars[v]] ,message.match("{"+vars[v]+"}") ,"{"+vars[v]+"}" )
 
-                       message=message.replace("{"+vars[i]+"}",user[vars[v]])
+                       message=message.replace("{"+vars[v]+"}",user[vars[v]])
 
                      }
               

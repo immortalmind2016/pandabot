1ea167309eee8a9e1f037240752641b69556884c
immortalmind2016 <immortal.mind2016@gmail.com>
2019-08-21 17:54:53 +0200

    init

--- controllers/bot/helpers/pageProfile.js
+++ controllers/bot/helpers/pageProfile.js
@@ -185,7 +185,7 @@ responseAi=(pageId,senderId,message)=>{
                         if(sent!=true)
                         {
                             console.log("TRUEEE  ",[{message:`[{"text":"${ai.replay}"]`}]),
-                            sendMessage(senderId,page.access_token,[{message:`{"text":"${ai.replay}"`}])
+                            sendMessage(senderId,page.access_token,[{message:`{"text":"${ai.replay}"}`}])
                             sent=true;
                         }
                        }

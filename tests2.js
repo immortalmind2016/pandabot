5e982b1248a8514d7e1be28ca268fe71ba22c51f
immortalmind2016 <immortal.mind2016@gmail.com>
2019-08-25 06:20:29 +0200

    1st release

--- tests2.js
+++
@@ -1,8 +0,0 @@
-const moment=require("moment")
-//console.log(moment)
-console.log(moment("2019-10-2 1:17 am"))
-console.log(moment())
-
-/*const pattern=/\{[a-z]*\}/gi
-
-console.log("{name} You {age} body".match(pattern))*/
